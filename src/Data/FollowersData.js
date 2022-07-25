a2c2af3c3de2690ad99dbe8b0466de0684e34656
MatyZala <mzx631@gmail.com>
2022-07-25 20:21:37 -0300

    Update

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,23 @@
+import img1 from '../img/img1.png'
+import img2 from '../img/img2.png'
+import img3 from '../img/img3.png'
+import img4 from '../img/img4.jpg'
+
+export const Followers = [
+    { name: 'Nombre',
+      username: 'Usuario',
+      img: img1
+    },
+    { name: 'Nombre',
+    username: 'Usuario',
+    img: img2
+    },
+    { name: 'Nombre',
+    username: 'Usuario',
+    img: img3
+    },
+    { name: 'Nombre',
+    username: 'Usuario',
+    img: img4
+    },   
+]
---
+++ src/Data/PostsData.js
@@ -0,0 +1,27 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.JPG'
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Nombre',
+        desc: 'descripcion',
+        likes: 2500,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Nombre',
+        desc: 'descripcion',
+        likes: 2500,
+        liked: false
+    },
+    {
+        img: postPic3,
+        name: 'Nombre',
+        desc: 'descripcion',
+        likes: 2500,
+        liked: true
+    }
+]

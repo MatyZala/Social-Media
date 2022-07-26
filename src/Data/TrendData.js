3b8246b717fd1cb5e89de815a6fbf1d166ea3795
MatyZala <mzx631@gmail.com>
2022-07-25 22:19:33 -0300

    Update

---
+++ src/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData = [
+    {
+        name: 'trend',
+        shares: 97,
+    },
+    {
+        name: 'trend2',
+        shares: 970,
+    },
+    {
+        name: 'trend3',
+        shares: 971,
+    },
+    {
+        name: 'trend4',
+        shares: 97,
+    },
+    {
+        name: 'trend',
+        shares: 97,
+    },
+]
--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -4,6 +4,9 @@ import Profile from '../../img/profileImg.jpg'
 import './ProfileCard.css'
 
 const ProfileCard = () => {
+
+
+    const ProfilePage = true;
   return (
     <div className="ProfileCard">
         <div className="ProfileImages">
@@ -27,15 +30,25 @@ const ProfileCard = () => {
                     <span>70</span>
                     <span>Followers</span>
                 </div>
+
+
+                {ProfilePage && (
+                    <>
+                    <div className="vl">
+
+                    </div>
+                    <div className="follow">
+                        <span>3</span>
+                        <span>Posts</span>
+                    </div>
+
+                    </>
+                )}
             </div>
             <hr />
         </div>
-
-        <span>
-            My Profile
-        </span>
-
-    </div>
+            {ProfilePage? '' : <span>My Profile</span>}
+        </div>
   )
 }
 
